a8583401cfd7892ad6dd9ac7eaef1c6a1165579d
Burakalper34 <52001691+Burakalper34@users.noreply.github.com>
2026-03-18 15:45:18 +0100

    Update cart.cy.js

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -1,18 +1,20 @@
-describe('Cart Tests', () => {
+describe("Cart Tests", () => {
   beforeEach(() => {
-    cy.fixture('users').as('users')
-  })
+    cy.visit("https://www.saucedemo.com/");
 
-  it('should add a product to the cart', function () {
-    cy.login(this.users.validUser.username, this.users.validUser.password)
-    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
-    cy.get('.shopping_cart_badge').should('contain', '1')
-  })
+    cy.fixture("users").then((user) => {
+      cy.login(user.validUser.username, user.validUser.password);
+    });
+  });
 
-  it('should remove a product from the cart', function () {
-    cy.login(this.users.validUser.username, this.users.validUser.password)
-    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
-    cy.get('[data-test="remove-sauce-labs-backpack"]').click()
-    cy.get('.shopping_cart_badge').should('not.exist')
-  })
-})
+  it("should add product to cart", () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('.shopping_cart_badge').should("contain", "1");
+  });
+
+  it("should remove product from cart", () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('[data-test="remove-sauce-labs-backpack"]').click();
+    cy.get('.shopping_cart_badge').should("not.exist");
+  });
+});
